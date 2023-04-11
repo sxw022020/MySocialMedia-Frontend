ec81cb9d48731efe1f08b9ae435c91e95bde4c59
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-04-11 01:23:28 -0700

    update index.js and deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// `ReactDOM.createRoot()` function with the DOM element
+// inside which we want the component to be rendered,
+// in this case the element with an ID of "root"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    // React application with the App component as the root, or first component
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
